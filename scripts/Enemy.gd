575574127de74ac68f96cdbbe15fc170445826ab
Zuberi Thompson <zthompson5@ggc.edu>
2024-09-30 09:31:42 -0400

    Enemy Class

---
+++ scripts/Enemy.gd
@@ -0,0 +1,19 @@
+# enemy.gd
+extends Node
+
+class_name Enemy
+
+var health: int
+var speed: float
+
+func _init(new_health: int, new_speed: float):
+	health = new_health
+	speed = new_speed
+
+func take_damage(damage: int):
+	health -= damage
+	if health <= 0:
+		die()
+
+func die():
+	queue_free()
