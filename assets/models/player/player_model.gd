bf9f09edef877d4149437cbef92294e52eaacd69
Malachi Wardrop <malachi.wardrop@gmail.com>
2024-10-10 14:19:06 -0400

    Adds New Player Model
    Adds in a new player model and animations.
    
    Animation setup is still a work in progress.

---
+++ assets/models/player/player_model.gd
@@ -0,0 +1,82 @@
+extends Node3D
+
+enum AnimationState {
+	IDLE, 
+	RUN,
+	JUMP
+}
+
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var _playback: AnimationNodeStateMachinePlayback = animation_tree["parameters/SM_Jump/playback"]
+@onready var skeleton: Skeleton3D = $Skeleton3D
+#@onready var right_hand := skeleton.find_bone("RightHand")
+
+var _move_direction = Vector3.ZERO : set = set_move_direction
+var _is_moving = false : set = set_is_moving
+#var in_air = false : set = set_in_air
+var _on_ground = true : set = set_on_ground
+var movement = 0.0 : set = set_movement
+var jump_shot = false : set = set_jump_shot
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animation_tree.active = true
+	
+func set_move_direction(direction: Vector3) -> void:
+	_move_direction = direction
+	update_movement_blend(direction)
+
+# Updates the float condition "movement" in the AnimationTree based on player direction
+func update_movement_blend(direction: Vector3) -> void:
+	# Set the "movement" parameter for the blend space (fight_idle and running)
+	if direction.length() == 0:
+		# Standing still
+		movement = 0.0
+	else:
+		# Forward or backward movement: adjust based on direction.z
+		movement = clamp(direction.z, -1.0, 1.0)  # Forward: 1.0, Backward: -1.0
+
+	# Update the "movement" parameter in the AnimationTree
+	#animation_tree["parameters/BlendTree/Movement/blend_position"] = movement
+	#transition_to(AnimationState.AIMING)
+
+func set_movement(value: float) -> void:
+	movement = value
+	animation_tree["parameters/Anim_Move/blend_position"] = value
+
+func set_is_moving(value: bool) -> void:
+	_is_moving = value
+	animation_tree["parameters/conditions/is_moving"] = value
+	
+#func set_in_air(value: bool) -> void:
+	#in_air = value
+	#animation_tree["parameters/conditions/in_air"] = value
+
+func set_jump_shot(value: bool):
+	jump_shot = value
+	animation_tree["parameters/Shot_Jump/request"] = "fire"
+	#animation_tree["parameters/Shot_Jump/active"] = value
+
+func set_on_ground(value: bool) -> void:
+	_on_ground = value
+	animation_tree["parameters/SM_Jump/conditions/_on_ground"] = value
+
+
+func get_bone(bone_name: String):
+	var bone = skeleton.find_bone(bone_name)
+	return bone
+
+## Transition to a new animation state
+#func transition_to(state_id: int) -> void:
+	#match state_id:
+		#AnimationState.IDLE:
+			#_playback.travel("idle")
+		##AnimationState.LAND:
+			##_playback.travel("air_land")
+		#AnimationState.RUN:
+			#_playback.travel("run")
+		##AnimationState.AIR:
+			##_playback.travel("air_jump")
+		#_:
+			#_playback.travel("idle")
